a73aeb0d59964b9705a41c492fc471b7824884cc
Yogeshwar Narsing Rekhawar <yogeshwarrekhawar@gmail.com>
2024-10-03 13:32:38 -0400

    Sentiment Analysis

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -11,11 +11,15 @@ urlpatterns = [
 
     # Path for login
     path('login', views.login_user, name='login'),  # Added trailing slash
-
+    path(route='get_dealers', view=views.get_dealerships, name='get_dealers'),
+    path(route='get_dealers/<str:state>', view=views.get_dealerships, name='get_dealers_by_state'),
     # Path for logout
     path('logout/', views.logout_user, name='logout'),
     path('get_cars/', views.get_cars, name='getcars'),
+    path(route='reviews/dealer/<int:dealer_id>', view=views.get_dealer_reviews, name='dealer_details'),
+    path(route='dealer/<int:dealer_id>', view=views.get_dealer_details, name='dealer_details'),
     # Paths for dealer reviews and adding reviews (to be implemented)
     # path('dealers/<int:dealer_id>/reviews/', views.get_dealer_reviews, name='dealer_reviews'),
     # path('add_review/', views.add_review, name='add_review'),
+    path(route='add_review', view=views.add_review, name='add_review'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
